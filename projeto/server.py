747860b4a2aa2ce9f87519fd257de2213cfa56c1
maksym <maksym121@hotmail.com>
2020-04-16 00:03:28 +0000

    funcoes basicas a funcionar, minor server bugfixes/parameter checking

--- projeto/server.py
+++ projeto/server.py
@@ -30,6 +30,9 @@ REG_NOT_OK  = 'client unsuccessfully registered'
 INV_CLIENT  = 'client is not registered' # invalid client
 INV_SESSION = 'client has no active session'  #no active session for the client"
 INV_MSG     = 'invalid message type'
+REG_USED    = 'client with that name already exists'
+HAS_SESSION = 'you already have a session, '
+EXIT        = 'you have ended your session'
 
 #generic functions
 
@@ -44,16 +47,18 @@ def register_client(msg_request, active_users, client_socket):
     name = msg_request[USER_ID]
     msg_reply = OK + REG_OK + "\n"
     # delete existing users
-    """
-    if name in active_users:
-        active_users.pop(name)
-        msg_reply = OK + REG_UPDATED + "\n"
     
-    dst_name = find_client(addr, active_users)
+    if name in active_users:
+        msg_reply = NOT_OK + REG_USED + "\n"
+        server_msg = msg_reply.encode()
+        return server_msg
+
+    dst_name = find_client(client_socket, active_users)
     if (dst_name != NULL):
-        active_users.pop(dst_name)
-        msg_reply = OK + REG_UPDATED + "\n"
-    """
+        msg_reply = NOT_OK + HAS_SESSION + dst_name + "\n"
+        server_msg = msg_reply.encode()
+        return server_msg
+    
     # register the user
     active_users[name] = client_socket
     server_msg = msg_reply.encode()
@@ -72,6 +77,13 @@ def forward_hello(msg_request, active_users, client_socket):
     dst_name = msg_request[USER_ID]
     src_name =  find_client(client_socket, active_users)
     server_msg = NOT_OK + INV_SESSION + "\n"
+    for key, val in list(active_users.items()):
+        if key == src_name:
+            break
+    if key != src_name:
+        server_reply = NOT_OK + INV_SESSION
+        server_reply = server_reply.encode()
+        return server_reply
     for key, val in list(active_users.items()):
         if key == dst_name:
             server_msg = 'HELLO'   + ' ' + dst_name + ' from ' + src_name +"\n"
@@ -92,7 +104,6 @@ def invalid_msg(msg_request):
   return server_msg
 
 
-
 def server_function(client_socket):
     while True:
         client_msg = client_socket.recv(MSG_SIZE)
@@ -106,9 +117,9 @@ def server_function(client_socket):
             server_msg = reply_hello(active_users, client_socket)
         elif(request_type == "HELLOTO"):
             server_msg = forward_hello(msg_request, active_users, client_socket)
-        elif(request_type == "KILLSERVER"):
+        elif(request_type == "EXIT"):
             server_msg = "KILL".encode()
-            server_sock.send(server_msg)
+            client_socket.send(server_msg)
             break
         else:
             server_msg = invalid_msg(msg_request)
@@ -119,22 +130,12 @@ def server_function(client_socket):
 
 #main code
 threads = []
-
-#connections = {}  guardar infos das ligacoes antes de fazerem register: formato -> 
-
 active_users = {} #dict: key: user_name; val:user_address info: example:'maria'= ('127.0.0.1',17234)
-
 # programa inicial, nao e thread
 server_sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
 server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # para depois poder matar com ctrl+c
-
 server_sock.bind(('', SERVER_PORT))
-
-
 server_sock.listen(5)
-
-
 
 
 while True:
@@ -145,30 +146,3 @@ while True:
     cliente = threading.Thread(target=server_function, args = (client_sock,))
     threads.append(cliente)
     cliente.start()
-
-
-
-#def server():
-"""
-        while True:
-        (client_msg, client_addr) = server_sock.recvfrom(MSG_SIZE)
-        print("Passou")
-        msg_request = client_msg.decode().split()
-        print(msg_request)
-        request_type = msg_request[TYPE]
-        if(request_type == "IAM"):
-            server_msg = register_client(msg_request, client_addr, active_users)
-        elif(request_type == "HELLO"):
-            server_msg = reply_hello(client_addr, active_users)
-        elif(request_type == "HELLOTO"):
-            (server_msg, client_addr) = forward_hello(msg_request, client_addr, active_users)
-        elif(request_type == "KILLSERVER"):
-            server_msg = "KILL".encode()
-            server_sock.send(server_msg)
-            break
-        else:
-            server_msg = invalid_msg(msg_request)
-        #server_sock.sendto(server_msg, (client_addr, SERVER_PORT))
-        server_sock.send(server_msg)
-    server_sock.close()
-    """
