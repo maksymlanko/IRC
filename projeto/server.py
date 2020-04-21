a9a6d71ea6d1b34656a475aa05ee8ea949c0b49e
maksym <maksym121@hotmail.com>
2020-04-21 00:27:39 +0000

    2 dicts -> dict + array; Can enter games

--- projeto/server.py
+++ projeto/server.py
@@ -18,6 +18,9 @@ MSG_SIZE = 1024
 #message info
 COMMAND = 0
 ARGUMENT = 1
+SOCKET  = 0
+STATUS  = 1
+INVITED = 2
 
 #return codes
 OK          = 'OK: '
@@ -28,7 +31,7 @@ REG_OK      = 'client successfully registered'
 REG_UPDATED = 'client registration updated'
 REG_NOT_OK  = 'client unsuccessfully registered'
 INV_CLIENT  = 'client is not registered' # invalid client
-INV_SESSION = 'client has no status session'  #no status session for the client"
+INV_SESSION = 'client has no user_infos session'  #no user_infos session for the client"
 INV_MSG     = 'invalid message type'
 REG_USED    = 'client with that name already exists'
 HAS_SESSION = 'you already have a session, '
@@ -43,19 +46,20 @@ ACCEPT      = 'you are in a game now!'
 ACCEPTED    = 'you are in a game now!'
 DECLINE     = 'you declined the game invite'
 DECLINED    = 'the player declined your invite'
+#GAME        = 'game has started'
 
 #generic functions
 
 def find_addr (addr): # pede addr
-    for key, val in list(users.items()):
-        if val == addr:
+    for key, val in list(user_infos.items()):
+        if val[SOCKET] == addr:
             return key
     return NULL
 
 def find_name(name): # pede name
-    for key, val in list(users.items()):
+    for key, val in list(user_infos.items()):
         if key == name:
-            return users[name]
+            return user_infos[name][SOCKET]
     return NULL
 
 
@@ -66,11 +70,10 @@ def register_client(msg_request, client_socket):
 
     if (client_name != NULL):
         msg_reply = NOT_OK + HAS_SESSION + client_name + "\n"
-    elif name in users: 
+    elif name in user_infos: 
         msg_reply = NOT_OK + REG_USED + "\n"
     else:
-        users[name] = client_socket
-        status[name] = FREE
+        user_infos[name] = [client_socket, FREE, NULL]
         msg_reply = OK + REG_OK + "\n"
 
     return msg_reply
@@ -107,21 +110,25 @@ def forward_hello(msg_request, client_socket):
 
 def show_status(client_socket):
     msg_reply = OK + '\n'
-    for key, val in list(status.items()): #fazer funcao
-        if status[key] == FREE:
+    for key, val in list(user_infos.items()): #fazer funcao
+        if user_infos[key][STATUS] == FREE:
             msg_reply += key + ': free\n'
-    for key, val in list(status.items()): #fazer funcao
-        if status[key] == BUSY:
+    for key, val in list(user_infos.items()): #fazer funcao
+        if user_infos[key][STATUS] == BUSY:
             msg_reply += key + ': busy\n'
+    for key, val in list(user_infos.items()): #fazer funcao
+        if user_infos[key][STATUS] == PLAYING:
+            msg_reply += key + ': playing\n'
     return msg_reply
 
 
 def invite(msg_request, client_socket):
     dst_name = msg_request[ARGUMENT]
-    status[dst_name] = BUSY
+    user_infos[dst_name][STATUS] = BUSY
     invited_socket = find_name(dst_name)
     src_name = find_addr(client_socket)
-    status[src_name] = BUSY
+    user_infos[dst_name][INVITED] = src_name
+    user_infos[src_name][STATUS] = BUSY
     server_reply = " You have been invited to a game by: " + src_name + ". (Y/n)"
     server_reply = server_reply.encode() #fazer uma funcao auxiliar que faz estas 2 linhas depois
     invited_socket.send(server_reply)
@@ -130,15 +137,24 @@ def invite(msg_request, client_socket):
     return msg_reply
 
 
-def update_status(accepted, client_socket):
-    dst_name = find_addr(client_socket)
-    
-    status[dst_name] = PLAYING # mudar todos if else para este formato?
-    msg_reply = OK + ACCEPT
-    if accepted == "N":
-        status[dst_name] = FREE
+def update_user_infos(accepted, client_socket):
+    src_name = find_addr(client_socket)
+    dst_addr = find_name(user_infos[src_name][INVITED])
+    dst_name = find_addr(dst_addr)
+    print(dst_name)
+    print(accepted)
+    if accepted == "Y":
+        user_infos[src_name][STATUS] = PLAYING # mudar todos if else para este formato?
+        user_infos[dst_name][STATUS] = PLAYING
+        msg_reply = OK + ACCEPT
+        server_reply = OK + ACCEPTED
+    else:
+        user_infos[src_name][STATUS] = FREE
+        user_infos[dst_name][STATUS] = FREE
         msg_reply = OK + DECLINE
-
+        server_reply = OK + DECLINED
+    server_reply = server_reply.encode()
+    dst_addr.send(server_reply)
     return msg_reply
 
 def invalid_msg(msg_request):
@@ -149,8 +165,7 @@ def invalid_msg(msg_request):
 
 def exit_session(client_socket):
     name = find_addr(client_socket)
-    del users[name]
-    del status[name]
+    del user_infos[name]
     return EXIT.encode()
 
 
@@ -172,8 +187,8 @@ def server_function(client_socket):
             server_msg = invite(msg_request, client_socket)
         elif(command.upper() == "Y" or command.upper() == "N"):
             client_name = find_addr(client_socket)
-            if status[client_name] == BUSY:
-                server_msg = update_status(command.upper(), client_socket)
+            if user_infos[client_name][STATUS] == BUSY:
+                server_msg = update_user_infos(command.upper(), client_socket)
         elif(command == "EXIT"):
             server_msg = exit_session(client_socket)
             server_msg = msg_reply.encode()
@@ -189,8 +204,8 @@ def server_function(client_socket):
 
 #main code
 threads = []
-users = {} #dict: key: user_name; val:user_address info: example:'maria'= ('127.0.0.1',17234)
-status = {}
+user_infos = {} #dict: key: user_name; val:user_address info: example:'maria'= ('127.0.0.1',17234)
+
 # programa inicial, nao e thread
 server_sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # para depois poder matar com ctrl+c
